32ec8e4d3efdc9731b480d4e4dfbf0b4937331e3
anglemiku <anglemiku.v@gmail.com>
2023-08-31 20:13:04 +0800

    Fix og parse

--- lib/src/public/main/model/og/og_image.g.dart
+++ lib/src/public/main/model/og/og_image.g.dart
@@ -11,6 +11,13 @@ OGImage _$OGImageFromJson(Map<String, dynamic> json) => OGImage(
       secureUrl: json['secure_url'] as String?,
       type: json['type'] as String?,
       alt: json['alt'] as String?,
-      width: json['width'] as int? ?? 0,
-      height: json['height'] as int? ?? 0,
+      width: _getInt(json['width']),
+      height: _getInt(json['height']),
     );
+
+int _getInt(dynamic amount) {
+      if (amount is int) return amount;
+      if (amount is String) return int.tryParse(amount) ?? 0;
+      if (amount is double) return amount.toInt();
+      return 0;
+}
